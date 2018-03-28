e7e5f0602df8e435e5cd0954ed68aa850e309ef9
mordowiciel <szymar95@gmail.com>
2018-03-29 00:30:23 +0200

    implementacja sygnalu trojkatnego

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from sines import *
 from szum import *
 from signals import *
 
-plot_prostokatny(15, 2, 0.5)
+plot_trojkatny(5, 2, 0.5)
--- signals.py
+++ signals.py
@@ -26,3 +26,56 @@ def plot_prostokatny(A, T, kW):
     plt.title('Szum gaussowski')
     plt.grid(True)
     plt.show()
+
+
+def plot_prostokatny_symetryczny(A, T, kW):
+    t = np.arange(0.0, 10.0, 0.01)
+    y = []
+
+    impulse_time = kW * T
+
+    for dt in t:
+
+        temp_t = dt % T
+
+        if temp_t <= impulse_time:
+            y_val = A
+
+        else:
+            y_val = -A
+
+        y.append(y_val)
+
+    plt.plot(t, y)
+    plt.xlabel('time (t)')
+    plt.ylabel('value (y)')
+    plt.title('Szum gaussowski')
+    plt.grid(True)
+    plt.show()
+
+
+def plot_trojkatny(A, T, kW):
+    t = np.arange(0.0, 10.0, 0.01)
+    y = []
+
+    impulse_time = kW * T
+
+    for dt in t:
+
+        temp_t = dt % T
+
+        if temp_t <= impulse_time:
+
+            y_val = (A / (kW * T)) * (temp_t)
+
+        else:
+            y_val = (-A / (T * (1 - kW))) * (temp_t) + A / (1 - kW)
+
+        y.append(y_val)
+
+    plt.plot(t, y)
+    plt.xlabel('time (t)')
+    plt.ylabel('value (y)')
+    plt.title('Szum gaussowski')
+    plt.grid(True)
+    plt.show()
