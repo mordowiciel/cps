a5765ed4186008eae4ed8d4710bf38749ba0818f
mordowiciel <szymar95@gmail.com>
2018-03-30 20:33:47 +0200

    fixed gaussian noise generator

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import signal_generator
+import noise_generator
+import plot_utils
 
 signal_generator.init_first_app_state()
+
+sig_gauss = noise_generator.gaussian(5, 0, 10, 100)
+plot_utils.plot_signal(sig_gauss, 'Uniform test')
