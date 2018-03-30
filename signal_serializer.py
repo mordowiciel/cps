816bfa3fc23e7e82fdd905fa874f637859bcced0
mordowiciel <szymar95@gmail.com>
2018-03-30 20:14:15 +0200

    create bin directory if it doesn't exist

--- signal_serializer.py
+++ signal_serializer.py
@@ -1,7 +1,11 @@
 import pickle
+import os
 
 
 def serialize_signal(sig, filename):
+    if not os.path.exists('bin/'):
+        os.makedirs('bin/')
+
     bin_file = open('bin/' + filename + '.bin', mode='wb')
     pickle.dump(sig, bin_file)
     bin_file.close()
